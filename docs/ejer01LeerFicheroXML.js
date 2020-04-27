d82fd18b544adc2eaf9f97c247283726e1a12f67
David <santacruzdl@gmail.com>
2020-04-27 10:25:30 +0200

    probando cambio1

--- docs/ejer01LeerFicheroXML.js
+++ docs/ejer01LeerFicheroXML.js
@@ -1,7 +1,7 @@
 function gestionarFicheroXML(xmlDoc){
 	
-	alert(xmlDoc.getElementsByTagName('libro')[0].firstChild.nodeValue)
-	alert(xmlDoc.getElementsByTagName('libro')[0].childNodes[1].firstChild.nodeValue)
+	alert(xmlDoc.getElementsByTagName('nombre')[0].firstChild.nodeValue)
+	alert(xmlDoc.getElementsByTagName('libro')[0].childNodes[2].firstChild.nodeValue)
 
 	document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent + "</b>"
 	
