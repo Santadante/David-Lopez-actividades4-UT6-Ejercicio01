f8ff71f8a7894c867a45a9aba470effa05ffd9b6
David <santacruzdl@gmail.com>
2020-04-27 10:47:17 +0200

    vemoa2

--- docs/ejer01LeerFicheroXML.js
+++ docs/ejer01LeerFicheroXML.js
@@ -1,8 +1,8 @@
 function gestionarFicheroXML(xmlDoc){
 	
 	alert(xmlDoc.getElementsByTagName('nombre')[0].firstChild.nodeValue) // 80 mundos
-	alert(xmlDoc.getElementsByTagName('libro')[0]).childNodes[2]
-	alert(xmlDoc.getElementsByTagName('libro')[0].childNodes[2].firstChild.nodeValue) // titulo libro
+	alert(xmlDoc.getElementsByTagName('libro')[0].childNodes[2])
+	alert(xmlDoc.getElementsByTagName('libro')[0].childNodes[2]) // titulo libro
 
 	document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent + "</b>"
 	
