6c89039bc1368ac5fc62e58167edfe4bf96e2aa1
David <santacruzdl@gmail.com>
2020-04-28 11:09:15 +0200

    probando6

--- docs/ejer01LeerFicheroXML.js
+++ docs/ejer01LeerFicheroXML.js
@@ -9,9 +9,9 @@ function gestionarFicheroXML(xmlDoc){
 		cogerDiv.className = "tabla";
 		cogerDiv.className = "fila";
 		cogerDiv.className = "celda";
-		cogerDiv.innerHTML += tit[i].firstChild.nodeValue + "<br>";
+		cogerDiv.innerHTML += tit[i].firstChild.nodeValue;
 		cogerDiv.className = "celda";
-		cogerDiv.innerHTML += aut[i].firstChild.nodeValue + "<br>";
+		cogerDiv.innerHTML += aut[i].firstChild.nodeValue;
 		if(valor < 25)
 			cogerDiv.className = "azul";
 		else
