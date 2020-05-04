fbe948650582c55be8ef3031ef98c44c983db21e
David <santacruzdl@gmail.com>
2020-05-04 13:13:20 +0200

    probando

---
+++ docs/Ejercicio02/ejer02LeerFicheroXML.js
@@ -0,0 +1,9 @@
+ function gestionarFicheroTXT(txtDoc){
+	let tit = txtDoc.getElementsByTagName('titulo');
+	
+}
+
+document.querySelector("div:nth-child(1)").addEventListener("click",()=>{
+
+    let xmlDoc = loadLDocA("mensajes.txt","txt");
+})
