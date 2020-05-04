8862e2be0f068cdf97c5253e2bfb6add9540dbd1
David <santacruzdl@gmail.com>
2020-05-04 16:55:30 +0200

    subiendo

--- docs/Ejercicio02/ejer02LeerFicheroXML.js
+++ docs/Ejercicio02/ejer02LeerFicheroXML.js
@@ -1,5 +1,11 @@
  function gestionarFicheroTXT(txtDoc){
-	
+	var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            var content = reader.result;
+            //Here the content has been read successfuly
+            alert(content);
+        }
 	
 }
 
