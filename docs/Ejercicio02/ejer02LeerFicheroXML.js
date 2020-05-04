1e8ed252d11d005750dc007dba477d4c407a0fae
David <santacruzdl@gmail.com>
2020-05-04 17:43:29 +0200

    subiendo

--- docs/Ejercicio02/ejer02LeerFicheroXML.js
+++ docs/Ejercicio02/ejer02LeerFicheroXML.js
@@ -1,12 +1,5 @@
  function gestionarFicheroTXT(txtDoc){
-    let fs = require(txtDoc);
-    let text = fs.readFileSync("./dancers.txt");
-    let textByLine = text.split("\n")
-    let names = read("dancers.txt").split("\n"); 
-    for (var i = 0; i < names.length; ++i) {
-          names[i] = names[i].trim();
-          console.log(names[i]);
-       }
+   
 }
 
 document.querySelector("div:nth-child(1)").addEventListener("click",()=>{
